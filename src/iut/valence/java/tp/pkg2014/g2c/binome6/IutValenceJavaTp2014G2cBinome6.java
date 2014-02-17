028245777dc2f5d58fc48e6e55da7a81991b5a1d
Bégot william <william.begot@iut-valence.fr>
2014-02-17 09:57:51 +0100

    Création du projet Démineur.

---
+++ src/iut/valence/java/tp/pkg2014/g2c/binome6/IutValenceJavaTp2014G2cBinome6.java
@@ -0,0 +1,16 @@
+package iut.valence.java.tp.pkg2014.g2c.binome6;
+
+/**
+ *
+ * @author begotw
+ */
+public class IutValenceJavaTp2014G2cBinome6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
