b169e6a21e29eef2f52313d787796d0a6f8cb701
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:23:07 +0100

    Add TODO (Case.java)

--- src/Demineur/Case.java
+++ src/Demineur/Case.java
@@ -1,3 +1,4 @@
+/* TODO Convention du java : les noms de package ne comportent pas de majuscules. */
 package Demineur;
 
 /**
@@ -5,31 +6,28 @@ package Demineur;
  *
  * @author begotw
  */
-public class Case
-{
-
-    // Définition des types:
+public class Case {
+    /* TODO Convention du Java : les noms de classes commencent par des majuscules. */
+    /* TODO Convention du Java : les valeurs des enum comme les constantes (ce qu'ils sont) ont des noms en majuscule */
     /**
      * Définition du type pour la définition du statut d'une case.
      */
-    private enum statutPossibleDeCase
-    {
+    private enum statutPossibleDeCase {
 
         marquee,
         masquee,
         decouvert;
     }
 
-    // Définition des varaibles:
     /**
      * Variable définissant si la case contient ou non une bombe.
      */
-    private boolean aUneBombe;
+    private boolean              aUneBombe;
     /**
      * Variable définissant le nombre de bombes adjacents à la case. Ce nombre
      * est compris entre 0 et 8
      */
-    private int aNBombesAdjacentes;
+    private int                  aNBombesAdjacentes;
     /**
      * Variable définissant l'état de la case. La case peut etre sous 3 états:
      * marquee, masquee ou decouvert
@@ -40,18 +38,19 @@ public class Case
     /**
      * Variable définissant la valeur par défaut si la case a une bombe ou non.
      */
-    private static final boolean AUNEBOMBE_DEFAUT = false;
+    private static final boolean              AUNEBOMBE_DEFAUT          = false;
     /**
      * Variable définissant la valeur par défaut du nombre de bombes adjacents
      * d'une case.
      */
-    private static final int ANBOMBESADJACENTES_DEFAUT = 0;
+    private static final int                  ANBOMBESADJACENTES_DEFAUT = 0;
     /**
      * Variable définissant l'état par défaut d'une case.
      */
-    private static final statutPossibleDeCase STATUTCASE_DEFAUT = statutPossibleDeCase.masquee;
+    private static final statutPossibleDeCase STATUTCASE_DEFAUT         = statutPossibleDeCase.masquee;
 
     // Définition des constructeurs:
+
     /**
      * Définition du constructeur de base.
      */
