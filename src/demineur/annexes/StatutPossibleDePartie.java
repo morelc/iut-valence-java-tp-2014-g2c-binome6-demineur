a4621f6bded8756916875a001fe1731307f736f9
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 15:34:04 +0100

    Add TODO (StatutPossibleDePartie.java)

--- src/demineur/annexes/StatutPossibleDePartie.java
+++ src/demineur/annexes/StatutPossibleDePartie.java
@@ -1,16 +1,14 @@
+/* TODO Changer le nom du package. */
 package demineur.annexes;
 
 /**
- * Statut d'une partie.
- * Enumeration contenant tous les statuts possibles pour une
- * partie.
+ * Tous les statuts possibles pour une partie.
  *
  * @author BEGOT William <william.begot@iut-valence.fr>
  * @version 0.1
  */
-public enum StatutPossibleDePartie
-{
-
+public enum StatutPossibleDePartie {
+    /* TODO Par convention en majuscule en Java. */
     enCours,
     estGagnee,
     estPerdue;
