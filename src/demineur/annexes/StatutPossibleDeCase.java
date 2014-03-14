df3d03e17674d6cdc23dc148e057e1d9914806d3
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 15:33:43 +0100

    Add TODO (StatutPossibleDeCase.java)

--- src/demineur/annexes/StatutPossibleDeCase.java
+++ src/demineur/annexes/StatutPossibleDeCase.java
@@ -1,16 +1,14 @@
+/* TODO Changer le nom du package. */
 package demineur.annexes;
 
 /**
- * Statut d'une case.
- * Enumeration contenant tous les statuts possibles pour une
- * case.
+ * Tous les statuts possibles pour une case.
  *
  * @author BEGOT William <william.begot@iut-valence.fr>
  * @version 0.1
  */
-public enum StatutPossibleDeCase
-{
-
+public enum StatutPossibleDeCase {
+    /* TODO Par convention en majuscule en Java. */
     marquee,
     masquee,
     decouvert;
